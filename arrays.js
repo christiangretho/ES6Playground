ed451cffca164fe150779d7e011f8f6ddace3e46
Christian B <christianbart13y@gmail.com>
2019-02-07 08:43:54 -0500

    pushed more array filter helper method examples

--- arrays.js
+++ arrays.js
@@ -44,7 +44,7 @@ var doubled = numbers.map(function(number) {
   return number * 2
 });
 
-console.log(doubled);
+// console.log(doubled);
 
 var images = [
   { height: '34px', width: '39px' },
@@ -72,3 +72,41 @@ function pluck(array, property) {
   })
   return newArray;
 }
+
+// filter helpers
+
+var products = [
+  {name: 'cucumber', type: 'vegetable', quantity: 0, price: 1},
+  {name: 'banana', type: 'fruit', quantity: 10, price: 15},
+  {name: 'celery', type: 'vegetable', quantity: 30, price: 13},
+  {name: 'orange', type: 'fruit', quantity: 3, price: 5}
+];
+
+var filteredProducts = [];
+
+for (var i = 0; i < products.length; i++){
+  if (products[i].type === 'fruit'){
+    filteredProducts.push(products[i]);
+  }
+}
+
+// console.log(filteredProducts);
+
+products.filter(function(product){
+  return product.type === 'fruit';
+});
+
+// type is 'vegetible', quantity is greater than 0, privae is less than 10
+
+products.filter(function(product){
+  return product.type === 'vegetable' 
+    && product.quantity > 0
+    && product.price < 10;
+});
+
+// Filter challenge
+var numbers = [15, 25, 35, 45, 55, 65, 75, 85, 95];
+
+var filteredNumbers = numbers.filter(function(number){
+  return number > 50;
+});
