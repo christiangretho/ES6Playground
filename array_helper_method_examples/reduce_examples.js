d983d8410dc2e179631a1aac9274286b82c51c1c
Christian B <christianbart13y@gmail.com>
2019-02-20 22:31:27 -0500

    more examples yada yada yada

--- array_helper_method_examples/reduce_examples.js
+++ array_helper_method_examples/reduce_examples.js
@@ -42,3 +42,24 @@ var totalDistance = trips.reduce(function(sum, trip){
   return sum + trip.distance;
 }, 0);
 
+// EXAMPLE 6
+var desks = [
+  { type: 'sitting' },
+  { type: 'standing' },
+  { type: 'sitting' },
+  { type: 'sitting' },
+  { type: 'standing' }
+];
+
+var deskTypes = desks.reduce(function(sum, desk) {
+  if (desk.type === 'sitting'){
+    ++sum.sitting;
+    } else {
+    ++sum.standing;
+    }
+    return sum;
+}, { sitting: 0, standing: 0 });
+
+// console.log(deskTypes);
+
+// EXAMPLE 7 CHALLENGE find and reduce
